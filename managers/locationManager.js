07e0af6ec24aa980bd9a12efc7bcd40e95a85cc6
ken <ken@bigMo>
2015-01-01 20:48:06 -0500

    updates

--- managers/locationManager.js
+++ managers/locationManager.js
@@ -100,7 +100,7 @@ exports.get = function(id, callback) {
                 var ArticleLocation = db.getArticleLocation();
                 ArticleLocation.find({location: location._id}, function(err, articleLocations) {
                     console.log("found article locations: " + JSON.stringify(articleLocations));
-                    if (!err && (articleLocations === undefined || articleLocations === null)) {
+                    if (!err && (articleLocations !== undefined && articleLocations !== null)) {
                         loc.articleList = articleLocations;
                     } else {
                         loc.articleList = [];
@@ -108,11 +108,12 @@ exports.get = function(id, callback) {
                     var ProductLocation = db.getProductLocation();
                     ProductLocation.find({location: location._id}, function(err, productLocations) {
                         console.log("found product locations: " + JSON.stringify(productLocations));
-                        if (!err && (productLocations === undefined || productLocations === null)) {
+                        if (!err && (productLocations !== undefined && productLocations !== null)) {
                             loc.productList = productLocations;
                         } else {
                             loc.productList = [];
                         }
+                        console.log("location returned: " + JSON.stringify(loc));
                         callback(loc);
                     });
                 });
