754ba10a365fb841f01961ae1587f2e51f025ffa
ken <ken@ken-Inspiron-5548>
2016-02-06 19:40:21 -0500

    new web serverside routing

--- initializers/webInitializer.js
+++ initializers/webInitializer.js
@@ -7,13 +7,13 @@ var contentController = require('../controllers/contentController');
 //var webTemplateUtility = require('../utils/webTemplateUtility');
 var angular = require('./angularJsInitializer');
 var fileRouteInitializer = require('./routes/fileRouteInitializer');
-var ejsRouteInitializer = require('./routes/ejsRouteInitializer');
+var webRouteInitializer = require('./routes/webRouteInitializer');
 exports.initialize = function (__dirname, self, cacheControlUtility, templateEngine) {
     console.log("template engine in web initializer: "+ JSON.stringify(templateEngine));
     fileRouteInitializer.fileInitializer(self);    
-    if(templateEngine.engine === "ejs"){
-        ejsRouteInitializer.initialize(self, cacheControlUtility, __dirname);
-    }
+    //if(templateEngine.engine === "ejs"){
+        webRouteInitializer.initialize(self, cacheControlUtility, __dirname);
+   // }
 
 };
 
